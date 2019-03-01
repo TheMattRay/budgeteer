7b6ef585d33db54bad934a7cd0801de036054fa7
Matt Ray <mray@onecause.com>
2019-03-01 10:37:45 -0500

    updates

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -45,49 +45,6 @@ const routes: Routes = [
         ]
       }
     ]
-  },
-  {
-    path: '',
-    component: TabsPage,
-    redirectTo: 'tabs',
-    children: [
-      {
-        path: 'budget',
-        children: [
-          {
-            path: '',
-            loadChildren: '../budget/budget.module#BudgetPageModule'
-          }
-        ]
-      },
-      {
-        path: 'income',
-        children: [
-          {
-            path: '',
-            loadChildren: '../income/income.module#IncomePageModule'
-          }
-        ]
-      },
-      {
-        path: 'transactions',
-        children: [
-          {
-            path: '',
-            loadChildren: '../transactions/transactions.module#TransactionsPageModule'
-          }
-        ]
-      },
-      {
-        path: '',
-        children: [
-          {
-            path: '',
-            loadChildren: '../budget/budget.module#BudgetPageModule'
-          }
-        ]
-      }
-    ]
   }
 ];
 
